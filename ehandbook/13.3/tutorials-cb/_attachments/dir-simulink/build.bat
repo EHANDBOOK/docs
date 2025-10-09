cfca055d832864732a7f8f4bc51a6c141e5d3f88
Service Account EHANDBOOK <138132559+EHANDBOOK@users.noreply.github.com>
2025-10-09 10:32:46 +0000

    Publish EHANDBOOK docs to GitHub.com

---
+++ ehandbook/13.3/tutorials-cb/_attachments/dir-simulink/build.bat
@@ -0,0 +1,23 @@
+C:\ETAS\EHANDBOOK-Container-Build_10.0.0-Windows\eHandbookCB.exe ^
+-i "EHB_Simulink_Example_V1" ^
+-o "." ^
+-n "EHB_Simulink_Example_V1" ^
+-nomatlab ^
+-gensvg
+
+C:\ETAS\EHANDBOOK-Container-Build_10.0.0-Windows\eHandbookCB.exe ^
+-i "EHB_Simulink_Example_V2" ^
+-o "." ^
+-n "EHB_Simulink_Example_V2" ^
+-nomatlab ^
+-gensvg
+
+C:\ETAS\EHANDBOOK-Container-Build_10.0.0-Windows\eHandbookCB.exe ^
+-i "EHB_Simulink_Example_V3" ^
+-o "." ^
+-n "EHB_Simulink_Example_V3" ^
+-nomatlab ^
+-gensvg ^
+-pdf
+
+pause
