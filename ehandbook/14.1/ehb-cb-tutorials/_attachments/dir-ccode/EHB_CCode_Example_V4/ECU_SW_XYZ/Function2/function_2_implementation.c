8417a5559e11312ff0b07015786fe4b9c183eb43
Service Account EHANDBOOK <138132559+EHANDBOOK@users.noreply.github.com>
2026-05-27 12:13:59 +0000

    Publish EHANDBOOK docs to GitHub.com

---
+++ ehandbook/14.1/ehb-cb-tutorials/_attachments/dir-ccode/EHB_CCode_Example_V4/ECU_SW_XYZ/Function2/function_2_implementation.c
@@ -0,0 +1,9 @@
+#include "shared_util.h"
+
+int computePower(void) {
+    int voltage = 12;
+    int current = 3;
+
+    // Same library function used here
+    return addValues(voltage, current);
+}
